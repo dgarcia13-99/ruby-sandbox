9c02b756da7fc7808d7792f5a80fb8bc62bf5410
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-07-10 18:25:37 +0000

    Used gems

---
+++ howdy.rb
@@ -0,0 +1,10 @@
+my_string = "Hello, world!"
+pp my_string
+
+require "./goodbye.rb"
+require "active_support/all"
+
+pp 1.ordinalize
+pp "table".pluralize
+pp "ruby".pluralize
+pp "tables".singularize
